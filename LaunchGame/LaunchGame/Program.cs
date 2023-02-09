748e92ae1f1760c26477b52af3328c569e08fca0
MattFiler <matthew.filer@hotmail.co.uk>
2023-02-09 23:46:14 +0000

    Port from OpenCAGE repo

---
+++ LaunchGame/LaunchGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaunchGame
+{
+    public static class SharedData
+    {
+        public static string pathToAI = "";
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Set paths
+            if (args.Length > 0 && args[0] == "-opencage") for (int i = 1; i < args.Length; i++) SharedData.pathToAI += args[i] + " ";
+            else SharedData.pathToAI = Environment.CurrentDirectory + " ";
+            SharedData.pathToAI = SharedData.pathToAI.Substring(0, SharedData.pathToAI.Length - 1);
+
+            //Verify location
+            if (!File.Exists(SharedData.pathToAI + "/AI.exe")) throw new Exception("This tool was launched incorrectly, or was not placed within the Alien: Isolation directory.");
+
+            //Run app
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LaunchGame());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Alien Isolation Launcher
+
+A tool to launch Alien: Isolation with various parameters and configs.
+
+Included in OpenCAGE.
