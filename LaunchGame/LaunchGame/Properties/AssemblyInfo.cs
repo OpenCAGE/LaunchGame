099d9e0cea7a46fa2be20132cff6092d5b604c45
MattFiler <matthew.filer@hotmail.co.uk>
2023-02-09 23:48:04 +0000

    Pull in submodule & rebuild

--- LaunchGame/LaunchGame/Properties/AssemblyInfo.cs
+++ LaunchGame/LaunchGame/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenCAGE")]
-[assembly: AssemblyDescription("An open-source set of community created tools for modifying Alien: Isolation.")]
+[assembly: AssemblyTitle("Launch Game")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matt Filer")]
-[assembly: AssemblyProduct("OpenCAGE")]
-[assembly: AssemblyCopyright("Copyright © Matt Filer 2017-23")]
+[assembly: AssemblyProduct("OpenCAGE: Launch Game")]
+[assembly: AssemblyCopyright("Copyright © Matt Filer 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.3.5")]
-[assembly: AssemblyFileVersion("0.9.3.5")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
