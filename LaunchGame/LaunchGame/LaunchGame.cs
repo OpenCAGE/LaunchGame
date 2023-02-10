55a0d950c4e8a25006c55a0f55846b7e35d8c4c1
MattFiler <matthew.filer@hotmail.co.uk>
2023-02-10 01:05:53 +0000

    Use new PAK2 parser

--- LaunchGame/LaunchGame/LaunchGame.cs
+++ LaunchGame/LaunchGame/LaunchGame.cs
@@ -1,4 +1,5 @@
-﻿using OpenCAGE;
+﻿using CATHODE;
+using OpenCAGE;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,6 +19,7 @@ namespace LaunchGame
     public partial class LaunchGame : Form
     {
         string cinematicToolDLL = "";
+        string utilPath = "";
 
         /* On init, if we are trying to launch to a map, skip GUI */
         public LaunchGame(string MapToLaunchTo = "")
@@ -30,12 +33,13 @@ namespace LaunchGame
             InitializeComponent();
 
             cinematicToolDLL = SettingsManager.GetString("PATH_GameRoot") + "/DATA/MODTOOLS/REMOTE_ASSETS/cinematictools/CT_AlienIsolation.dll";
+            utilPath = SettingsManager.GetString("PATH_GameRoot") + "/DATA/MODTOOLS/REMOTE_ASSETS/runtimeutils/";
 
             enableCinematicTools.Checked = SettingsManager.GetBool("OPT_CinematicTools");
             enableCinematicTools.Enabled = SettingsManager.GetString("META_GameVersion") == "STEAM" && File.Exists(cinematicToolDLL);
 
             enableRuntimeUtils.Checked = SettingsManager.GetBool("OPT_Runtime_Utils");
-            enableRuntimeUtils.Enabled = SettingsManager.GetString("META_GameVersion") == "STEAM";
+            enableRuntimeUtils.Enabled = SettingsManager.GetString("META_GameVersion") == "STEAM" && File.Exists(utilPath);
 
             enableUIPerf.Checked = SettingsManager.GetBool("OPT_cUIEnabled_UIPerf");
             enableUIPerf.Enabled = SettingsManager.GetString("META_GameVersion") != "WINDOWS_STORE";
@@ -96,7 +100,6 @@ namespace LaunchGame
             {
                 try
                 {
-                    string utilPath = SettingsManager.GetString("PATH_GameRoot") + "/DATA/MODTOOLS/REMOTE_ASSETS/runtimeutils/";
                     File.Copy(utilPath + "OpenCAGE_Utils.asi", rtUtilASI, true);
                     File.Copy(utilPath + "winmm.dll", rtUtilDLL, true);
                 }
@@ -158,7 +161,7 @@ namespace LaunchGame
         }
 
         /* UI Modifications */
-        //CathodeLib.PAK AlienPAK = new CathodeLib.PAK();
+        PAK2 AlienPAK = null;
         private void UIMOD_DebugCheckpoints_CheckedChanged(object sender, EventArgs e)
         {
             UpdateUI("PAUSEMENU", UIMOD_DebugCheckpoints.Checked);
@@ -177,15 +180,20 @@ namespace LaunchGame
         }
         private void UpdateUI(string file, bool modded)
         {
-            //if (AlienPAK.Format != CATHODE.PAKType.PAK2) AlienPAK.Open(SettingsManager.GetString("PATH_GameRoot") + "/DATA/UI.PAK");
+            if (AlienPAK == null)
+                AlienPAK = new PAK2(SettingsManager.GetString("PATH_GameRoot") + "/DATA/UI.PAK");
 
-            FileStream stream = File.Create(file);
-            GetResourceStream((modded) ? "UI_Mods/" + file + "_MOD.GFX" : "UI_Mods/" + file + ".GFX").CopyTo(stream);
-            stream.Close();
+            using (MemoryStream stream = new MemoryStream())
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                GetResourceStream((modded) ? "UI_Mods/" + file + "_MOD.GFX" : "UI_Mods/" + file + ".GFX").CopyTo(stream);
+                reader.BaseStream.Position = 0;
+                PAK2.File pakFile = AlienPAK.Entries.FirstOrDefault(o => o.Filename == "DATA/UI/" + file + ".GFX");
+                if (pakFile != null)
+                    pakFile.Content = reader.ReadBytes((int)reader.BaseStream.Length);
+            }
 
-            //AlienPAK.ImportFile("DATA/UI/" + file + ".GFX", file);
-            File.Delete(file);
-
+            AlienPAK.Save();
             SettingsManager.SetBool("UIOPT_" + file, modded);
         }
         protected static Stream GetResourceStream(string resourcePath)
@@ -235,8 +243,6 @@ namespace LaunchGame
             {
                 return false;
             }
-
-            cinematicToolInjectTask.Dispose();
         }
 
         //Everything below is thanks to: https://github.com/ihack4falafel/DLL-Injection/blob/master/DllInjection/DllInjection/Program.cs
